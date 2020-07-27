e958a328f5513996f48190f8a2a88acfdde2ff3a
scott89 <scott89@github.com>
2020-07-27 22:24:35 +0800

    working on kitti dataset

--- core/datasets/kitti.py
+++ core/datasets/kitti.py
@@ -4,41 +4,11 @@ import torch
 from torch.utils.data import Dataset
 from PIL import Image
 from config.defult_config import config
+import numpy as np
 
 
-
-def _get_depth_path(im_path):
-    depth_path = im_path.replace('image_02/data', 'proj_depth/velodyne/image_02')
-    depth_path = depth_path.replace('png', 'npz')
-    return depth_path
-
-def _get_context_path(im_file, backward_context, forward_context, stride=1):
-    parent_folder = os.path.dirname(im_file)
-    base_name, ext = os.path.splitext(os.path.basename(im_file))
-    c_id = int(base_name)
-    # get backward context
-    backward_context_paths = []
-    for i in range(1, backward_context+1):
-        b_id = c_id - i * stride
-        b_path = join(parent_folder, str(b_id).zfill(len(base_name)) + ext)
-        if os.path.exists(b_path):
-            backward_context_paths.append(b_path)
-        else:
-            return None, None
-
-    # get forward context
-    forward_context_paths = []
-    for i in range(1, backward_context+1):
-        f_id = c_id + i * stride
-        f_path = join(parent_folder, str(f_id).zfill(len(base_name)) + ext)
-        if os.path.exists(f_path):
-            forward_context_paths.append(f_path)
-        else:
-            return None, None
-
-    return backward_context_paths, forward_context_paths
-
-    
+FOLDER = {'left': 'image_02', 
+          'right':'image_04'}
 
 
 class KITTI(Dataset):
@@ -50,6 +20,7 @@ class KITTI(Dataset):
         self.with_depth = with_depth
         self.with_pose = with_pose
         self.data_file = join(data_path, data_file)
+        self.calibration_cache = []
         if with_context:
             self.backward_context_paths = []
             self.forward_context_paths = []
@@ -63,7 +34,7 @@ class KITTI(Dataset):
             if not with_depth:
                 im_paths.append(path)
             else:
-                depth_path =  _get_depth_path(path)
+                depth_path =  self.get_depth_path(path)
                 if depth_path is not None and os.path.exists(depth_path):
                     im_paths.append(path)
                     depth_paths.append(depth_path)
@@ -75,7 +46,7 @@ class KITTI(Dataset):
             depth_with_context_paths = []
             for idx, path in enumerate(im_paths):
                 backward_context_path, forward_context_path = \
-                        _get_context_path(path, backward_context, forward_context, stride)
+                        self.get_context_path(path, backward_context, forward_context, stride)
                 if backward_context_path is not None:
                     im_with_context_paths.append(path)
                     self.forward_context_paths.append(forward_context_path)
@@ -88,6 +59,86 @@ class KITTI(Dataset):
         self.im_paths = im_paths
         if with_depth:
             self.depth_paths = depth_paths
+
+    def __len__(self):
+        return len(self.im_paths)
+
+    def __getitem__(self, idx):
+        im_path = self.im_paths[idx]
+        data = {'image': Image.open(im_path)}
+        # get intrinsics
+        c_data = self.get_calibration(im_path)
+        intrinsics = self.get_intrinsics(c_data, im_path)
+        data.update({'intrinsics': intrinsics})
+        # get pose
+        if self.with_pose:
+            data['pose'] = 
+
+    
+    @staticmethod
+    def get_depth_path(im_path):
+        for cam in FOLDER.values():
+            if cam in im_path:
+                depth_path = im_path.replace(cam+'/data', 'proj_depth/velodyne/'+cam)
+                depth_path = depth_path.replace('png', 'npz')
+                return depth_path
+    
+    @staticmethod
+    def get_context_path(im_file, backward_context, forward_context, stride=1):
+        parent_folder = os.path.dirname(im_file)
+        base_name, ext = os.path.splitext(os.path.basename(im_file))
+        c_id = int(base_name)
+        # get backward context
+        backward_context_paths = []
+        for i in range(1, backward_context+1):
+            b_id = c_id - i * stride
+            b_path = join(parent_folder, str(b_id).zfill(len(base_name)) + ext)
+            if os.path.exists(b_path):
+                backward_context_paths.append(b_path)
+            else:
+                return None, None
+    
+        # get forward context
+        forward_context_paths = []
+        for i in range(1, backward_context+1):
+            f_id = c_id + i * stride
+            f_path = join(parent_folder, str(f_id).zfill(len(base_name)) + ext)
+            if os.path.exists(f_path):
+                forward_context_paths.append(f_path)
+            else:
+                return None, None
+    
+        return backward_context_paths, forward_context_paths
+
+    def get_calibration(self, im_path):
+        parent_path = os.path.dirname(im_path)+'../../../../'
+        if parent_path in self.calibration_cache:
+            c_data = self.calibration_cache[parent_path]
+        else:
+            c_data = self.read_calibration_file(parent_path)
+        return c_data
+    
+    @staticmethod
+    def read_calibration_file(parent_path):
+        file_path = join(parent_path, 'b_cam_to_cam.txt')
+        c_data = dict()
+        with open(file_path, 'r') as fid:
+            for line in fid.readlines():
+                key, value = line.split(':', 1)
+                try:
+                    c_data[key] = np.array([float(i) for i in value.split()])
+                except ValueError:
+                    pass
+        return c_data
+
+    @staticmethod
+    def get_intrinsics(c_data, im_path):
+        for cam in FOLDER.values():
+            if cam in im_path:
+                intrinsics = c_data[cam.replace('Image', 'P_rect')]
+                intrinsics = np.reshape(intrinsics, [3, 4])[:, :4]
+                return intrinsics
+     
 
 
 
